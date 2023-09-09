a26c6de968af9d7952ec2da450ae95b4dc3f36fe
Pratik Fuse <pratikfuse20@gmail.com>
2023-09-09 14:38:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 //your code here
 
+let touristSpots = ["The Virupaksha Temple", "Victoria Memorial", "Tajmahal"];
+let withoutArticleArray = [];
+
+let map = {};
+
+for (let i = 0; i < touristSpots.length; i++) {
+  let wordWithoutArticle = touristSpots[i].replace(/\bthe\b/gi, "");
+  wordWithoutArticle = wordWithoutArticle.trim();
+  withoutArticleArray.push(wordWithoutArticle);
+
+  map[wordWithoutArticle] = touristSpots[i];
+}
+
+withoutArticleArray.sort();
+let ul = document.getElementById("bands");
+for (let i of withoutArticleArray) {
+  let li = document.createElement(li);
+  li.innerHTML = map[i];
+  ul.appendChild(li);
+}
